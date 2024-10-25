555b30fdba98443ebc36907bf6f0f8840ca7bc0a
SMshoukat <smshoukat11@gmail.com>
2024-10-25 16:19:40 +0500

    Added Data Table and Dialogs

--- src/app/(admin)/admin/courses/page.js
+++ src/app/(admin)/admin/courses/page.js
@@ -1,7 +1,17 @@
+import { CourseTable } from "@/components/DataTables/CourseTable";
+import { CourseDialog } from "@/components/Dialogs/CourseModals";
+import { Button } from "@/components/ui/button";
+
+
 export default function Courses() {
     return (
       <div className="min-h-screen p-10">
-        <h1 className="text-4xl font-bold text-center">Courses</h1>
+        <div className="flex justify-between my-2">
+        <h1 className="text-3xl font-bold text-center">Courses</h1>
+        <CourseDialog/>
+
+        </div>
+        <CourseTable/>
       </div>
     );
   }
--- src/app/(admin)/admin/layout.js
+++ src/app/(admin)/admin/layout.js
@@ -1,18 +1,43 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Link from "next/link"
 
 
-export default function Layout({children}) {
-    return (
+
+export default function Layout({children}){
+    return(
         <div>
-    <Tabs defaultValue="account" className="w-[400px]">
-        <TabsList>
-            <TabsTrigger value="account">Account</TabsTrigger>
-            <TabsTrigger value="password">Password</TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">Make changes to your account here.</TabsContent>
-        <TabsContent value="password">Change your password here.</TabsContent>
-    </Tabs>
+        
+        <Tabs defaultValue="dashboard" className="w-full">
+            <TabsList className="w-full">
+
+                <Link href={`/admin/dashboard`}>
+                <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
+                </Link>
+
+                <Link href={`/admin/courses`}>
+                <TabsTrigger value="courses">Courses</TabsTrigger>
+                </Link>
+
+                <Link href={`/admin/batches`}>
+                <TabsTrigger value="batches">Batches</TabsTrigger>
+                </Link>
+
+                <Link href={`/admin/trainers`}>
+                <TabsTrigger value="trainers">trainers</TabsTrigger>
+                </Link>
+
+                <Link href={`/admin/student`}>
+                <TabsTrigger value="student">Student</TabsTrigger>
+                </Link>
+                    
+            </TabsList>
+            <TabsContent value="dashboard">{children} </TabsContent>
+            <TabsContent value="courses">{children} </TabsContent>
+            <TabsContent value="batches">{children} </TabsContent>
+            <TabsContent value="trainers">{children} </TabsContent>
+            <TabsContent value="student">{children} </TabsContent>
+        </Tabs>
 
         </div>
-    );
-  }
+    )
+}
